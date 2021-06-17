be7347df29c9175b293fb0d51a3c28519a9f0866
mmarienh <Sp-714539361>
2021-06-17 15:11:55 +0200

    Updated Readme and added check for correct paths in extension.ts

--- README.md
+++ README.md
@@ -2,12 +2,41 @@
 
 ## Beschreibung
 
-Dieses Repository enthält den Sourcecode für das Divekit VSCode Plugin. Zusammen mit den Repositorys divekit-language-plugin-vscode und divekit-language-server bildet es die Codebasis für das Praxisprojekt "Entwicklung eines IDE Plugins zur Unterstützung bei der Erstellung individualisierter Praktika-und Klausuraufgaben".
+Dieses Repository enthält den Sourcecode für das Divekit VSCode Plugin. Zusammen mit den Repositorys [divekit-language-plugin-intellij](https://github.com/divekit/divekit-language-plugin-intellij) 
+und [divekit-language-server](https://github.com/divekit/divekit-language-server) bildet es die Codebasis für das Praxisprojekt "Entwicklung eines IDE Plugins zur Unterstützung bei der Erstellung individualisierter Praktika-und Klausuraufgaben".
 
-Eine ausführliche Dokumentation der verschiedenen Projektbestandteile befindet sich im Wiki.
+Eine ausführliche Dokumentation der verschiedenen Projektbestandteile befindet sich im [Wiki](https://github.com/divekit/divekit-language-server/wiki).
 
 ## Nutzung
 
+Das Plugin ist für die Nutzung im Projekt [automated-repo-setup](https://github.com/divekit/divekit-automated-repo-setup) entwickelt worden.
+
+Nach der Installation sind noch folgende Schritte erforderlich:
+
+* Divekit Language Server JAR muss auf dem lokalen Dateisystem vorhanden sein.
+
+* Der absolute Pfad zur JAR muss in den Einstellungen unter Settings -> Extensions -> Divekit eingetragen werden.
+
+* Der absolute Pfad zu den Dateien `variationsConfig.json` und `variableExtensionsConfig.json` muss in den Einstellungen
+  eingetragen werden.
+  Achtung: Das Plugin startet den Language Server nur, wenn die beiden Config-Dateien gefunden werden.
+
+* Anschließend kann das Projekt `automated-repo-setup` gestartet werden. Wenn es vorher schon geöffnet war, muss es einmal geschlossen
+  und wieder geöffnet werden. Wenn unten rechts die Meldung erscheint, dass der Language Server in diesem Projekt genutzt werden kann, hat
+  alles funktioniert.
+  
+* Wenn man in einem Markdown- oder Java-File zwischen der Code-Completion für das Platzhaltervariablen System und der Code-Completion
+für dementsprechend Java oder Markdown wechseln möchte, kann man dies über die Symbolleister unterhalb des Editorfensters von VSCode. Nach einem Klick kann
+  hier eine Sprache ausgewählt werden.
+
 ## Weiterentwicklung & Anpassungen
+
+* Probleme & Fehler können auf GitHub zuerst unter der [Issues](https://github.com/divekit/divekit-language-plugin-vscode/issues) eingetragen werden
+  und anschließend von da abgearbeitet werden.
+
+* Vor dem Mergen empfiehlt sich in der Regel ein Pull-Request, da so die Code-Qualität hoch gehalten werden kann.
+
+* Im Reiter Run & Debug (CTRL + SHIFT + D) lässt sich über "Run Extension" (F5) eine neue
+  VSCode Instanz mit installierter Extension zum schnellen Testen der Funktionalität öffnen.
 
 ## Testen
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as path from 'path';
+import * as fs from 'fs';
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
 
 // Name of the launcher class which contains the main.
@@ -10,31 +10,41 @@ const main: string = 'StdioLauncher';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "divekit" is now active!');
 
 	let pathToJar: string | undefined = vscode.workspace.getConfiguration().get('divekit.paths.pathToLanguageServerJar');
 	let pathToVariationsConfig: string | undefined = vscode.workspace.getConfiguration().get('divekit.paths.pathToVariationsConfigJson');
 	let pathToExtensionsConfig: string | undefined = vscode.workspace.getConfiguration().get('divekit.paths.pathToVariableExtensionsConfigJson');
 
 	if(pathToJar === undefined || pathToJar.length <= 0) {
-		vscode.window.showInformationMessage('Settings: Please do not forget to enter the absolute path to the Divekit Language Server Jar!');
+		vscode.window.showInformationMessage('Settings: Bitte den absoluten Pfad zur Divekit Language Server Jar eingeben!');
 		return;
 	}
 	if(pathToVariationsConfig === undefined || pathToVariationsConfig.length <= 0) {
-		vscode.window.showInformationMessage('Settings: Please do not forget to enter the absolute path to the variationsConfig.json!');
+		vscode.window.showInformationMessage('Settings: Bitte den absoluten Pfad zur variationsConfig.json eingeben!');
 		return;
 	}
 	if(pathToExtensionsConfig === undefined || pathToExtensionsConfig.length <= 0) {
-		vscode.window.showInformationMessage('Settings: Please do not forget to enter the absolute path to the variableExtensionsConfig.json!');
+		vscode.window.showInformationMessage('Settings: Bitte den absoluten Pfad zur variableExtensionsConfig.json eingeben!');
 		return;
 	}
+
+	let jarExists: boolean = false;
+	let variationsConfigExists: boolean = false;
+	let variableExtensionsConfigExists: boolean = false;
+	
+	fs.existsSync(pathToJar) ? jarExists = true : vscode.window.showInformationMessage('Divekit Language Server JAR nicht gefunden. Pfad richtig angegeben?');
+	fs.existsSync(pathToVariationsConfig) ? variationsConfigExists = true : vscode.window.showInformationMessage('variationsConfig.json nicht gefunden. Pfad richtig angegeben?');
+	fs.existsSync(pathToExtensionsConfig) ? variableExtensionsConfigExists = true : vscode.window.showInformationMessage('variableExtensionsConfig.json nicht gefunden. Pfad richtig angegeben?');
 
 	// Get the java home from the process environment.
 	const { JAVA_HOME } = process.env;
 
-	console.log(`Using java from JAVA_HOME: ${JAVA_HOME}`);
-	// If java home is available continue.
-	if (JAVA_HOME) {
+	if(!JAVA_HOME) {
+		vscode.window.showInformationMessage('Das JAVA_HOME Environment wurde nicht in der Prozessumgebung gefunden. Bitte fügen Sie JAVA_HOME zu Ihren Systemumgebungsvariablen hinzu.');
+	}
+
+	// If JAVA_HOME and all needed files are present, start the language server
+	if (JAVA_HOME && jarExists && variationsConfigExists && variableExtensionsConfigExists) {
 		const args: string[] = ["-jar", pathToJar, pathToVariationsConfig, pathToExtensionsConfig];
 
 		// Set the server options 
@@ -55,12 +65,11 @@ export function activate(context: vscode.ExtensionContext) {
 		// Create the language client and start the client.
 		let disposable = new LanguageClient('divekit', 'Divekit Language Server', serverOptions, clientOptions).start();
 
-		vscode.window.showInformationMessage('Divekit Language Server started!');
+		vscode.window.showInformationMessage('Der Divekit Language Server wurde gestartet!');
 
 		// Disposables to remove on deactivation.
 		context.subscriptions.push(disposable);
-	}
+	}	
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
